cd294920e6e920f06f28e0fdef542694a71a342d
joedicandia <dicandiajoseph@gmail.com>
2019-08-13 03:20:26 +0000

    updated

--- createCrisprReady.sh
+++ createCrisprReady.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-for file in /home/dicaj/week4/newexomes/*
+for file in exomes/*
 do
 
+trimmedfilename="$(echo $file | sed 's/.fasta//')"
 while read motif
 do
 echo -n $motif
 echo -n "	"
 trimmedmotif="$(echo $motif | tr -d [:space:])"
 grep -o $trimmedmotif $file | wc -l
-done < "$1" | sort -k2,2nr | head -n 3 >> $file"_topmotifs.fasta"
+done < "$1" | sort -k2,2nr | head -n 3 | cut -f1 >> $trimmedfilename"_topmotifs.fasta"
 
 done
