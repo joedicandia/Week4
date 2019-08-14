8f4dad85d7549f396445abe5f0519cba1ca573c6
joedicandia <dicandiajoseph@gmail.com>
2019-08-14 01:04:35 +0000

    added exomereport python script

---
+++ exomeReport.py
@@ -0,0 +1,28 @@
+print("Organism CHICKEN, Discovered by DISCOVERER, has a diameter DIAMETER, and has the NUMBER of genes in common with the cohort, and the following genes unique to only itself:")
+chickencrispr=open("./exomespostcrispr/chicken_postcrispr.fasta", "r")
+print(chickencrispr.read())
+
+print("Organism DROMEDARY, Discovered by DISCOVERER, has a diameter DIAMETER, and has the NUMBER of genes in common with the cohort, and the following genes unique to only itself:")
+dromedarycrispr=open("./exomespostcrispr/dromedary_postcrispr.fasta", "r")
+print(dromedarycrispr.read())
+
+print("Organism FOX, Discovered by DISCOVERER, has a diameter DIAMETER, and has the NUMBER of genes in common with the cohort, and the following genes unique to only itself:")
+foxcrispr=open("./exomespostcrispr/fox_postcrispr.fasta", "r")
+print(foxcrispr.read())
+
+print("Organism GOAT, Discovered by DISCOVERER, has a diameter DIAMETER, and has the NUMBER of genes in common with the cohort, and the following genes unique to only itself:")
+goatcrispr=open("./exomespostcrispr/goat_postcrispr.fasta", "r")
+print(goatcrispr.read())
+
+print("Organism GOPHER, Discovered by DISCOVERER, has a diameter DIAMETER, and has the NUMBER of genes in common with the cohort, and the following genes unique to only itself:")
+gophercrispr=open("./exomespostcrispr/gopher_postcrispr.fasta", "r")
+print(gophercrispr.read())
+
+print("Organism GORILLA, Discovered by DISCOVERER, has a diameter DIAMETER, and has the NUMBER of genes in common with the cohort, and the following genes unique to only itself:")
+gorillacrispr=open("./exomespostcrispr/gorilla_postcrispr.fasta", "r")
+print(gorillacrispr.read())
+
+print("Organism LAMB, Discovered by DISCOVERER, has a diameter DIAMETER, and has the NUMBER of genes in common with the cohort, and the following genes unique to only itself:")
+lambcrispr=open("./exomespostcrispr/lamb_postcrispr.fasta", "r")
+print(lambcrispr.read())
+
